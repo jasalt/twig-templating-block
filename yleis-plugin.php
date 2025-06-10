9b1d032467a60aeaad2499fd4af070e8fe20e7f2
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-06-10 12:25:44 +0300

    WIP timber-block

--- yleis-plugin.php
+++ yleis-plugin.php
@@ -8,6 +8,17 @@
    Author URI: https://netura.fi
  */
 
-require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php'); // TODO check if Twig is available, run if not AI!
+// Check if Twig is available, if not load from vendor
+if (!class_exists('\Twig\Environment')) {
+    if (file_exists(plugin_dir_path(__FILE__) . 'vendor/autoload.php')) {
+        require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php');
+    } else {
+        add_action('admin_notices', function() {
+            echo '<div class="notice notice-error"><p>Yleis Plugin: Twig library not found. Please run "composer install" in the plugin directory.</p></div>';
+        });
+        return;
+    }
+}
 
 require("blocks/dynamic-template/block.php");
+// require("blocks/twig-binding/block.php");  // uses only Twig, no Timber dependency
